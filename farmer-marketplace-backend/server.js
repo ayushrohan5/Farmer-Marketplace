ec4ed3b77732b37c4e44bb7d088f1b1468289bbb
Ayush Rohan <ayushrohan5@gmail.com>
2025-04-24 15:54:14 +0530

    final page

--- farmer-marketplace-backend/server.js
+++ farmer-marketplace-backend/server.js
@@ -25,6 +25,10 @@ app.use('/api/cart', cartRoutes);
 app.use('/api/payment', paymentRoutes);
 app.use("/api/contact", contactRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Server is running');
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
